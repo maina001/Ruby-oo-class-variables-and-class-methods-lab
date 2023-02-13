edab88deee30cbbbcf574d66bb05f74f224202ce
Brian <brianmakorious@gmail.com>
2023-02-13 18:28:07 +0300

    def  song

---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+class Song
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  attr_reader :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
